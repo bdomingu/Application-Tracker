010575064a18e0f134803941cf30791eb72467b4
bdomingu <beatrizdominguez14@gmail.com>
2022-07-15 19:10:49 -0600

    save

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mysql = require("mysql");
+
+const app = express()
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "LoginSystem",
+
+});
+
+app.listen(3001, () => {
+    console.log("Server started in port 3001")
+})
